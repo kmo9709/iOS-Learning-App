2ec91e263cf0af18308afcbce9ab3accbbd1fd06
Kevin Mahoney <kmahoney9709dev@gmail.com>
2024-12-18 12:35:10 -0600

    adding Resume App

---
+++ Resume/Resume/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Resume
+//
+//  Created by Mahoney, Kevin (STL) on 12/18/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
